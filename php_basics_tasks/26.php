ec555320f2b6594b76c1e8f711961181e0310218
Skazatel <35700244+Skazatel@users.noreply.github.com>
2018-01-31 03:17:44 +0200

    Add files via upload

---
+++ php_basics_tasks/26.php
@@ -0,0 +1,7 @@
+<?php
+define('DAYS_COUNT', 7);
+echo DAYS_COUNT . '<br/>';
+const MONTH_COUNT = 12;
+echo MONTH_COUNT;
+// 26. Объявите константу DAYS_COUNT равную 7 и константу MONTH_COUNT равную 12 двумя разными способами объявления констант
+?>
